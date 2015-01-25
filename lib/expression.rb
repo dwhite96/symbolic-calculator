6af0f0d3722f4e58fd722be0d82ec267d673a596
David White <whited75@gmail.com>
2015-01-25 00:25:55 -0700

    Symbolic calculator v1.0 commit

--- lib/expression.rb
+++ lib/expression.rb
@@ -12,6 +12,21 @@ class Expression
   # @param bindings [Hash] a map of variable names to concrete values
   # @return [Expression] Returns a possibly-simplified Expression
   def evaluate(bindings = {})
-    "Implement Expression#evaluate in #{__FILE__}"
+    stack = []
+    @expr.split.each do |token|
+      case token
+      when /[a-z]+/
+        token = token.to_sym
+        if bindings.include?(token)
+          stack.push(bindings[token])
+        end
+      when /\d/
+        stack.push(token.to_i)
+      when "+", "-", "*", "/"
+        operands = stack.pop(2)
+        stack.push(operands[0].send(token, operands[1]))
+      end
+    end
+    stack
   end
 end
